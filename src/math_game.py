20c03b87719dbe5861222870094468bce48f186a
varshamadapoosi <varshamadapoosi@berkeley.edu>
2021-12-20 15:20:29 -0800

    math game

--- src/math_game.py
+++ src/math_game.py
@@ -1,47 +1,44 @@
 
-
 import random
 import operator
 from operator import add, sub, mul, truediv
 
-vals = {'1' : "Addition", "2": "Subtraction", "3": "Multiplication", "4": "Divsion"}
-opvals = {'1' : "+", "2": "-", "3": "x", "4": "/"}
-ops = {'1' : add, "2": sub, "3": mul, "4": truediv}
+
+"""global variables to be accessed by all classes about the operations"""
+operation_words = {'1' : "Addition", "2": "Subtraction", "3": "Multiplication", "4": "Divsion"}
+operation_symbols = {'1' : "+", "2": "-", "3": "x", "4": "/"}
+operation_function = {'1' : add, "2": sub, "3": mul, "4": truediv}
 score_overall  = {'1' : 0, "2": 0, "3": 0, "4": 0}
 
-
 class Driver:
-
-	def __init__(self, name):
-		self.name = name
+	"""Performs play options of the game"""
+	def __init__(self):
 		self.operation = None
-	#	self.vals = ['1', "2", "3", "4"]
-	#	self.dict = {'1' : "Addition", "2": "Subtraction", "3": "Multiplication", "4": "Divsion"}
 		self.valid = False
 		self.reply = False
-		self.rounds =1
+		self.rounds = 1
 		self.detailed_rounds = {'1' : 0, "2": 0, "3": 0, "4": 0}
 
-
-	
 	def drive(self):
 
 		operation = self.choose_operation()
-		q = Questions(self.name , operation)
-		s = Score(self.name)
+		q = Questions(operation)
+		s = Score()
 		for i in range(10):
-			val = q.random_questions(operation)
+			val = q.random_questions(operation, i+1)
 			s.score_keeping(val)
 		s.print_round_score()
 		self.replay()
 
 	def choose_operation(self):
+		"""user inputs which operation they want to practice"""
+
 		self.valid = False
 		while self.valid == False:
 			operation = input("Which operation would you like to practice? Press 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division: ")
-			if operation in vals.keys():
+			if operation in operation_words.keys():
 				self.valid = True
-				print("You selected: " + vals[operation] + ". It's time to practice!")
+				print("You selected: " + operation_words[operation] + ". It's time to practice!")
 				self.detailed_rounds[operation] +=1
 
 
@@ -66,8 +63,11 @@ class Driver:
 				print("You didn't select a valid option. Try Again!")
 					'''
 	def replay(self):
+
+		"""if user wants to replay their game or if not, displays the final score"""
+
 		self.reply = False
-		questions_wrong_sum = 0
+		
 		while self.reply == False:
 			repl = input("Do you want to replay? Type 'yes' to continue or 'no' to exit the game    ")
 			if repl in ["yes", "no"]:
@@ -76,31 +76,39 @@ class Driver:
 					self.rounds +=1
 					self.drive()
 				else:
-					print('You played ' + str(self.rounds) + " rounds!")
-					print('Here is a breakdown of your score')
-
-					for item in score_overall:
-						questions_wrong_sum += score_overall[item]
-						print("For " + vals[item] + ", you missed " + str(score_overall[item]) + " questions out of " + str(self.detailed_rounds[item] * 10))
+					self.end_game(self.rounds, )
 					
-
-
-					print("In total, you got " + str(questions_wrong_sum) + " out of " + str(self.rounds * 10)+ " total questions")
-					return
 			else:
 				print("You didn't select a valid option. Try Again!")
 
 	
+	def end_game(self, rounds):
+		questions_wrong_sum = 0
+		print('You played ' + str(rounds) + " rounds!")
+		print('Here is a breakdown of your score')
+
+		for item in score_overall:
+			questions_wrong_sum += score_overall[item]
+			print("For " + operation_words[item] + ", you missed " + str(score_overall[item]) + " questions out of " + str(self.detailed_rounds[item] * 10))
+		
+		print("In total, you got " + str(questions_wrong_sum) + " out of " + str(rounds * 10)+ " total questions")
+		return
 
 
 
 class Questions:
-	def __init__(self, name, operation):
+
+	"""class where the questions are created """
+
+	def __init__(self, operation):
 	#	self.ops = {'1' : add, "2": sub, "3": mul, "4": truediv}
 	#	self.opvals = {'1' : "+", "2": "-", "3": "x", "4": "/"}
 		self.operation = operation
 
-	def random_questions(self, operation ):
+	def random_questions(self, operation, num ):
+
+		"""generates random questions from random numbers """
+
 		v = False;
 		input_int = False
 		while v == False:
@@ -112,11 +120,11 @@ class Questions:
 			if (operation == "4" and one//two == one/two) or operation in ["1", "2", "3"]:
 				v = True
 				while input_int == False:
-					prompt = input("What is the result of " + str(one) + " " + str(opvals[operation]) + " " + str(two) + "?                    ")
+					prompt = input(str(num) +  ") What is the result of " + str(one) + " " + str(operation_symbols[operation]) + " " + str(two) + "?                    ")
 				
 					if prompt.isnumeric():
 						input_int = True
-						if int(prompt) == ops[operation](one, two):
+						if int(prompt) == operation_function[operation](one, two):
 							return True
 						else:
 							return [False, one, two, operation]
@@ -126,43 +134,53 @@ class Questions:
 
 
 class Score:
-	def __init__(self, name):
-		self.name = name
+
+	"""score class"""
+
+	def __init__(self):
+
 		self.score = 0
 		self.questions_wrong_list = []
 		self.questions_wrong_answers_list = []
 
 	def score_keeping(self, answer):
+		"""adds up the scores if right"""
+
 		if answer == True:
 			self.score +=1
 		else:
-			self.print_questions_wrong(answer)
+			self.adds_questions_wrong(answer)
 
 	def print_round_score(self):
+		"""prints the wrong questions below the round score"""
+
 		print("Your score was " + str(self.score) + " out of 10!")
 		print("The Questions you missed with the correct answers were: ")
 
-		score_overall[operation] += len(self.questions_wrong_list)
+		
 		
 		for i in range(len(self.questions_wrong_list)):
 			print(str(self.questions_wrong_list[i]) + " = " + str(self.questions_wrong_answers_list[i]))
 
 
-	def print_questions_wrong(self, answer):
+	def adds_questions_wrong(self, answer):
+		"""adds wrong questions into a list """
+
+
 		one = answer[1]
 		two = answer[2]
 		operation = answer[3]
 
-		self.questions_wrong_list.append(str(one) + " " + str(opvals[operation]) + " " + str(two))
-		self.questions_wrong_answers_list.append( ops[operation](one, two))
+		self.questions_wrong_list.append(str(one) + " " + str(operation_symbols[operation]) + " " + str(two))
+		self.questions_wrong_answers_list.append( int(operation_function[operation](one, two)))
 		
-		
+		score_overall[operation] += 1
 
 
 
 
 if __name__ == "__main__":
-	d = Driver("varsha")
+	d = Driver()
 	d.drive()
 
 
